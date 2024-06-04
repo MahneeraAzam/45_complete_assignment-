123e21ba6e90a2849bae22eb5efc8c98d0409f71
MahneeraAzam <minheraazam403@gmail.com>
2024-06-04 09:53:14 -0700

    Q12

---
+++ Q_12/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let frndsNames = ["Aiman", "Mindi", "Arshi", "Mamur"];
+let msg = ("Nice to meet u");
+for (let index = 0; index < frndsNames.length; index++) {
+    console.log(`${msg}`, frndsNames[index]);
+}
---
+++ Q_12/index.ts
@@ -0,0 +1,9 @@
+
+
+
+let frndsNames :string[] = ["Aiman","Mindi","Arshi","Mamur"];
+
+let msg :string = ("Nice to meet u");
+for (let index = 0; index < frndsNames.length; index++) {
+    console.log(`${msg}`, frndsNames[index]);
+}
