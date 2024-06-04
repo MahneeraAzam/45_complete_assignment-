5a9a24d2e464ac97bf4661670f6754e8376fd42e
MahneeraAzam <minheraazam403@gmail.com>
2024-06-04 08:49:09 -0700

    q9

---
+++ Q_9/index.ts
@@ -0,0 +1,8 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+let favoriteNumber :string = "10";
+let msg :string = `This is my fvrt number ${favoriteNumber}`
+console.log(msg);
+
+
