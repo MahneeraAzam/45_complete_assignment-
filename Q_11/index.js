57b10ae2f241bf96cd6074088c50b16617fd25f9
MahneeraAzam <minheraazam403@gmail.com>
2024-06-04 09:39:58 -0700

    q11

---
+++ Q_11/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+Object.defineProperty(exports, "__esModule", { value: true });
+let frndsNames = ["Aiman", "Mindi", "Arshi", "Mamur"];
+for (let index = 0; index < frndsNames.length; index++) {
+    console.log(frndsNames[index]);
+}
---
+++ Q_11/index.ts
@@ -0,0 +1,6 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let frndsNames :string[] = ["Aiman","Mindi","Arshi","Mamur"];
+for (let index = 0; index < frndsNames.length; index++) {
+    console.log(frndsNames[index]);
+}
