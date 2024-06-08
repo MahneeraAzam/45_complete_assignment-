23dc18143ad4f5125730eb31b1859b098386f64b
MahneeraAzam <minheraazam403@gmail.com>
2024-06-08 08:28:47 -0700

    Q_21

---
+++ Q_21/index.ts
@@ -0,0 +1,15 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+type Tobj = {
+    name: string,
+    age: number,
+    isStudent: boolean
+}
+
+let obj : Tobj = {
+    name:  "Mindi",
+    age: 19,
+    isStudent: true
+}
+console.log(obj);
+
