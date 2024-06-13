10e8f64a2982888ea30d1275a8a0e6197e6e93c9
MahneeraAzam <minheraazam403@gmail.com>
2024-06-13 09:06:59 -0700

    Q_thirtyone

---
+++ Q_31/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+Object.defineProperty(exports, "__esModule", { value: true });
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you again.for logging in again.
+let usernames = ["Admin", "Mindi", "Mahneera", "Arshi", "Mamur"];
+while (usernames.length > 0) {
+    usernames.pop();
+}
+if (usernames.length === 0) {
+    console.log(" We need to find some users!");
+}
