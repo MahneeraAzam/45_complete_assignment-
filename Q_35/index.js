40e1283d626726a88d59a3187aa24f9e48dea1a6
MahneeraAzam <minheraazam403@gmail.com>
2024-06-14 02:41:19 -0700

    'q

---
+++ Q_35/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
+Object.defineProperty(exports, "__esModule", { value: true });
+let Animals = ["Cat", "Dog", "Crocodile"];
+for (let index = 0; index < Animals.length; index++) {
+    console.log(`A ${Animals[index]} would make a great pet.`);
+}
+console.log("\n Any of these animals would make a great pet!");
